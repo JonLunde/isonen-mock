c1f304b5f9123874ec3e009984fec62ce14cbce5
JonMLunde <jonlund95@gmail.com>
2021-08-10 01:43:05 +0200

    toml update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -118,11 +118,9 @@ const IndexPage = () => (
 // Static data-fetching
 export async function getStaticProps() {
   const apolloClient = initializeApollo();
-
   await apolloClient.query({
     query: CHARACTERS_BY_IDS_QUERY,
   });
-
   return addApolloState(apolloClient, {
     props: {},
     revalidate: 1,
